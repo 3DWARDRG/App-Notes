e5d2b1e1774ffeb60e0554b8555bc667c4442721
JoseHerCast <jose@ptree.com.mx>
2023-08-24 02:09:34 -0600

    Frontend del aplicativo

--- backend/models/Notes.js
+++
@@ -1,9 +0,0 @@
-const { Schema, model } = require('mongoose');
-
-const NoteSchema = new Schema({
-    id: { type: Number, required: true, unique: true },
-    titulo: { type: String, required: true },
-    contenido: { type: String, required: true },
-}, { timestamps: true });
-
-module.exports = model('Note', NoteSchema);
